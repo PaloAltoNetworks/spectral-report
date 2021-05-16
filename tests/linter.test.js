ca8deaa2fb745d07607261d8e257849a5d9f1f70
Abhiram Hemanna <abhiram.mh@gmail.com>
2021-05-16 16:24:47 +0530

    Fixed linter.js test cases.

--- tests/linter.test.js
+++ tests/linter.test.js
@@ -3,13 +3,16 @@ const { loadFile } = require("../lib/utils");
 const { Spectral } = require("@stoplight/spectral");
 const path = require("path");
 
-jest.mock("@stoplight/spectral", () => {
+jest.mock("@stoplight/spectral", function () {
     return {
-        Spectral: jest.fn().mockImplementation(() => {
+        Spectral: jest.fn().mockImplementation(function () {
+            this.registerFormat = jest.fn().mockImplementation((param, fn) => { });
+            this.loadRuleset = jest.fn().mockImplementation((param) => { return Promise.resolve(() => { }) });
+            this.run = jest.fn().mockImplementation((param) => { return new Promise((resolve) => { resolve([]) }); });
             return {
-                registerFormat: jest.fn((param, fn) => { }),
-                loadRuleset: jest.fn((param) => { return Promise.resolve(() => { }) }),
-                run: jest.fn((param) => { return Promise.resolve(() => { return []; }) })
+                registerFormat: this.registerFormat,
+                loadRuleset: this.loadRuleset,
+                run: this.run
             }
         })
     };
@@ -23,47 +26,43 @@ jest.mock("../lib/utils.js", () => {
     };
 });
 
+beforeEach(() => {
+    Spectral.mockClear();
+});
+
 afterEach(() => {
-    // jest.resetAllMocks();
     jest.restoreAllMocks();
 });
 
 describe("linter", () => {
     it("lint() - without ruleset", async () => {
-        const spectral = new Spectral();
-
-        await lint("/fake/spec/path");
+        let fakeSpecPath = "/fake/spec/path";
+        let output = await lint(fakeSpecPath);
 
         expect(Spectral).toHaveBeenCalled();
 
-        // TODO: Fix the test cases
-
-        // expect(spectral.registerFormat).toHaveBeenCalled();
-
-        // await expect(spectral.loadRuleset.mock.results[0]).resolves;
-        // expect(spectral.loadRuleset).toHaveBeenCalled();
-
-        // await expect(spectral.run.mocks.results[0]).resolves;
-        // expect(spectral.run).toHaveBeenCalled();
-
+        expect(Spectral.mock.instances[0].registerFormat).toHaveBeenCalledTimes(2);
+        expect(Spectral.mock.instances[0].loadRuleset).toHaveBeenCalledTimes(1);
+        expect(Spectral.mock.instances[0].loadRuleset).toHaveBeenCalledWith("spectral:oas");
+        expect(Spectral.mock.instances[0].run).toHaveBeenCalledTimes(1);
+        expect(Spectral.mock.instances[0].run).toHaveBeenCalledWith("");
         expect(loadFile).toHaveBeenCalled();
+        expect(output).toEqual([]);
     });
 
     it("lint() - with ruleset", async () => {
-        const spectral = new Spectral();
-
-        await lint("/fake/spec/path", "/fake/rules/path");
+        let fakeSpecPath = "/fake/spec/path";
+        let fakeRulesPath = "/fake/rules/path";
+        let output = await lint(fakeSpecPath, fakeRulesPath);
 
         expect(Spectral).toHaveBeenCalled();
-        // TODO: Fix the test cases
 
-        // expect(spectral.registerFormat).toHaveBeenCalled();
-
-        // await expect(spectral.loadRuleset.mock.results[0]).resolves;
-        // expect(spectral.loadRuleset).toHaveBeenCalled();
-
-        // await expect(spectral.run.mocks.results[0]).resolves;
-        // expect(spectral.run).toHaveBeenCalled();
+        expect(Spectral.mock.instances[0].registerFormat).toHaveBeenCalledTimes(2);
+        expect(Spectral.mock.instances[0].loadRuleset).toHaveBeenCalledTimes(1);
+        expect(Spectral.mock.instances[0].loadRuleset).toHaveBeenCalledWith(fakeRulesPath);
+        expect(Spectral.mock.instances[0].run).toHaveBeenCalledTimes(1);
+        expect(Spectral.mock.instances[0].run).toHaveBeenCalledWith("");
         expect(loadFile).toHaveBeenCalled();
+        expect(output).toEqual([]);
     });
 });
